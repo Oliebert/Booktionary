e645bf70d534e646f852f23f6dd3c9bc0b122a4a
olga liebert <olga.liebert@gmail.com>
2016-11-09 18:15:27 +0100

    pyPDF2 converter

--- pdfconverter.py
+++ pdfconverter.py
@@ -1,26 +1,25 @@
 # coding=utf-8
 
 #Converting from .pdf with PyPDF2 package
-
-import PyPDF2
-
-class PdfConverter:
-
-    def getPDFContent(path):
-        content =""
-        # Load PDF into pyPDF
-        pdf = PyPDF2.PdfFileReader(open(path, "rb"))
-        # Iterate pages
-        for i in range(0, pdf.getNumPages()):
-            # Extract text from page and add to content
-            content += pdf.getPage(i).extractText() #+ "\n"
-        # Collapse whitespace
-        content = u" ".join(content.replace(u"\xa0", u" ").strip().split())
-        content.encode('utf-8')
-        return content
-    print (getPDFContent("mail_cir.pdf").encode('utf-8'))
-
 '''
+import codecs
+import PyPDF2
+def getPDFContent(path):
+    content = ""
+    # Load PDF into pyPDF
+    pdf = PyPDF2.PdfFileReader(open(path, "rb"))
+    # Iterate pages
+    for i in range(0, pdf.getNumPages()):
+        # Extract text from page and add to content
+        content += pdf.getPage(i).extractText() + "\n"
+    # Collapse whitespace
+    content = " ".join(content.replace(u"\xa0", " ").strip().split())
+    return content
+print (getPDFContent("mail.pdf").encode("ascii", "ignore"))
+
+    #codecs.open(os.path.join(path, filename[0]), 'r', encoding='utf-8') as f:
+
+
 
 from pyPdf import PdfFileReader, PdfFileWriter
 pdf = pyPdf.PdfFileReader(open('mail.pdf', "rb"))
@@ -28,3 +27,10 @@ for page in pdf.pages:
     print (page.extractText())
 '''
 
+import PyPDF2
+pdf_file = open('mail_cir.pdf', 'rb')
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+number_of_pages = read_pdf.getNumPages()
+page = read_pdf.getPage(0)
+page_content = page.extractText()
+print (page_content.decode('cp1251').encode('utf-8'))
